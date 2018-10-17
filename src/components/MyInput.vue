a674e7137b0c487997088076dbd6afbe463b9d03
Кирилл Кучеров <cvkucherov@yandex.ru>
2018-10-17 12:09:20 +0300

    Input and coverage

---
+++ src/components/MyInput.vue
@@ -0,0 +1,38 @@
+<template>
+    <input
+    class="Field"
+    :pattern="pattern"
+    :value="text"
+    />
+</template>
+
+<script>
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        name: 'MyInput',
+
+        props: {
+            pattern: {
+                type: String,
+                default: '[+\-]?(?:0|[1-9]\d*)(?:\.\d*)?(?:[eE][+\-]?\d+)?',
+            },
+            text: {
+                type: String,
+                required: true,
+            },
+        },
+    });
+</script>
+
+<style lang="scss" scoped>
+    @import '@/assets/main.scss';
+    @import '@/assets/mixins.scss';
+
+    .Field {
+        @include border-radius-1px;
+
+        background-color: white;
+        color: $primary-color;
+    }
+</style>
