3661d0a3403f002ace002b9c322c597346f2fadc
Кирилл Кучеров <cvkucherov@yandex.ru>
2018-10-17 12:04:23 +0300

    Button

---
+++ src/components/MyButton.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="MyButon__container" data-jest="element-to-click" @click="handleClick">
+        <p>{{ text }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        name: 'MyButton',
+        
+        methods: {
+            handleClick() {
+                this.$emit(this.eventName, this.meta);
+            },
+        },
+
+        props: {
+            eventName: {
+                type: String,
+                default: 'my-button-click',
+            },
+            meta: {
+                type: String,
+                required: true,
+            },
+            text: {
+                type: String,
+                required: true,
+            },
+        },
+    });
+</script>
+
+<style lang="scss" scoped>
+    @import '@/assets/main.scss';
+    @import '@/assets/mixins.scss';
+
+    .MyButton__container {
+        @include border-radius-1px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: white;
+        color: $primary-color;
+
+        width: 40px;
+        height: 40px;
+        margin: 5px;
+
+        &:hover {
+            background-color: $primary-color;
+            color: white;
+        }
+    }
+</style>
---
+++ tests/unit/components/MyButton.spec.ts
@@ -0,0 +1,81 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+
+import MyButton from '@/components/MyButton.vue';
+import faker from 'faker';
+
+const localVue = createLocalVue();
+
+describe('MyButton', () => {
+    describe('component', () => {
+        let meta: String;
+        let text: String;
+
+        beforeEach(() => {
+            meta = faker.random.uuid();
+            text = faker.random.uuid();
+        });
+
+        it('should call handleClick if component was clicked', async () => {
+            const handleClick = jest.fn();
+            const wrapper: any = shallowMount(MyButton, {
+                propsData: {
+                    meta,
+                    text,
+                },
+                methods: {
+                    handleClick,
+                },
+            });
+
+            wrapper.find('[data-jest="element-to-click"]').trigger('click');
+
+            expect(handleClick).toBeCalled();
+        });
+    });
+
+    describe('methods', () => {
+        describe('handleClick', () => {
+            let meta: String;
+            let text: String;
+
+            beforeEach(() => {
+                meta = faker.random.uuid();
+                text = faker.random.uuid();
+            });
+
+            it('should emit default event if no eventName provided', () => {
+                const wrapper: any = shallowMount(MyButton, {
+                    localVue,
+                    propsData: {
+                        meta,
+                        text,
+                    },
+                });
+
+                wrapper.vm.handleClick();
+
+                const emitted = wrapper.emitted()[wrapper.vm.eventName];
+                expect(emitted).toBeTruthy();
+                expect(emitted[0]).toEqual([meta]);
+            });
+
+            it('should emit eventName if it is provided', () => {
+                const eventName = faker.random.uuid();
+                const wrapper: any = shallowMount(MyButton, {
+                    localVue,
+                    propsData: {
+                        eventName,
+                        meta,
+                        text,
+                    },
+                });
+
+                wrapper.vm.handleClick();
+
+                const emitted = wrapper.emitted()[eventName];
+                expect(emitted).toBeTruthy();
+                expect(emitted[0]).toEqual([meta]);
+            });
+        });
+    });
+});
